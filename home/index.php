d58fc567556919f69f8d1239f113bcce070c701b
VASSEUR Maxime <Maxime.VASSEUR@akka.eu>
2019-10-08 15:00:23 +0200

    home + login

---
+++ home/index.php
@@ -0,0 +1,17 @@
+<?php require '../layout/header.php'; ?>
+
+<head>
+  <script src="https://unpkg.com/ag-grid-enterprise/dist/ag-grid-enterprise.min.noStyle.js"></script>
+  <link rel="stylesheet" href="https://unpkg.com/ag-grid-community/dist/styles/ag-grid.css">
+  <link rel="stylesheet" href="https://unpkg.com/ag-grid-community/dist/styles/ag-theme-balham.css">
+</head>
+
+<!-- <div class="col-md-10 col-md-offset-1 spacer-below-20">
+    <label>The date</label>
+    <div class="input-group">
+        <input class="datepicker" type="text" name="date">
+    </div>
+</div> -->
+<h2>PAGE D'ACCUEIL</h2>
+
+<?php require '../layout/footer.php'; ?>
