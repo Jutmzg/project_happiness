89e47c8da81d95efb70fc74a7736922a7bca2482
VASSEUR Maxime <Maxime.VASSEUR@akka.eu>
2019-10-08 17:59:08 +0200

    datepicker en FR

--- layout/footer.php
+++ layout/footer.php
@@ -2,12 +2,24 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
 <script>
-    $(function() {
-        $('.datepicker').datepicker({
-            dateFormat: 'yy/mm/dd'
-
-        });
-    });
+$(function() {
+$( ".datepicker" ).datepicker({
+	closeText: 'Fermer',
+	prevText: 'Précédent',
+	nextText: 'Suivant',
+	currentText: 'Aujourd\'hui',
+	monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+	monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+	dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+	dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
+	dayNamesMin: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+	weekHeader: 'Sem.',
+	dateFormat: 'yy/mm/dd'
+	});
+});
+		  $(function() {
+			$( ".datepicker" ).datepicker();
+		  });
 </script>
 </body>
 
