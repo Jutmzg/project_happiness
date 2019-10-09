6534f3dbf8606ff417439d42746bbb91203b53da
BOUSSEAU Juliette <Juliette.BOUSSEAU@akka.eu>
2019-10-09 14:36:55 +0200

    export terminé

--- export.php
+++ export.php
@@ -58,14 +58,50 @@ $spreadsheet->getActiveSheet()->setCellValue('A1', 'Liste des Missions')
         ->setCellValue('F2', 'Début de la Mission')
         ->setCellValue('G2', 'Fin de la Mission')
         ->setCellValue('E2', 'Statut');
-        $row = 3;
 
-header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+$sql = 'SELECT * FROM customer';
+$statement = $connection->prepare($sql);
+$statement->execute();
+$customers = $statement->fetchAll(PDO::FETCH_OBJ);
+$spreadsheet->createSheet(2);
+$spreadsheet->setActiveSheetIndex(2);
+$row = 3;
+    foreach($customers as $value){
+        $spreadsheet->getActiveSheet()
+        ->setCellValue('B'.$row, $value->name)
+        ->setCellValue('C'.$row, $value->address)
+        ->setCellValue('D'.$row, $value->state);
+    $row++;     
+    }
+$spreadsheet->getActiveSheet()->setTitle('Client');
+$spreadsheet->getActiveSheet()->setCellValue('A1', 'Liste des Clients')
+    ->setCellValue('B2', 'Nom')
+    ->setCellValue('C2', 'Adresse')
+    ->setCellValue('D2', 'Statut');
+
+$sql = 'SELECT * FROM job';
+$statement = $connection->prepare($sql);
+$statement->execute();
+$jobs = $statement->fetchAll(PDO::FETCH_OBJ);
+$spreadsheet->createSheet(3);
+$spreadsheet->setActiveSheetIndex(3);
+$row = 3;
+    foreach($jobs as $value){
+        $spreadsheet->getActiveSheet()
+            ->setCellValue('B'.$row, $value->name);
+        $row++;     
+    }
+$spreadsheet->getActiveSheet()->setTitle('Métier');
+$spreadsheet->getActiveSheet()->setCellValue('A1', 'Liste des Métiers')
+    ->setCellValue('B2', 'Nom');
+
+header('Content-Type: text/xlsx; charset=UTF-8');
 header('Content-Disposition: attachment; filename="Akkappiness.xlsx"');
 header('Cache-Control: max-age=0');
 header('Cache-Control: max-age=1');
 header('Cache-Control: cache, must-revalidate');
 header('Pragma: public');
+header('Content-Encoding: UTF-8');
 
 $writer = new Xlsx($spreadsheet);
 $writer->save('php://output');
