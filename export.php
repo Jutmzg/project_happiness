eeb8908cf57d2960090d92d7a33707854b4564bf
BOUSSEAU Juliette <Juliette.BOUSSEAU@akka.eu>
2019-10-09 12:15:13 +0200

    one sheet

--- export.php
+++ export.php
@@ -12,25 +12,44 @@ $consultants = $statement->fetchAll(PDO::FETCH_OBJ);
 
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
-$column = 'A';
-$row = 7;
-foreach($consultants as $c)
-        {   
-          
-            $sheet
-                ->setCellValue($column . $row, 'test');
-                
-        }
+$row = 3;
+foreach($consultants as $value){
+        $sheet
+            ->setCellValue('B'.$row, $value->lastname)
+            ->setCellValue('C'.$row, $value->firstname)
+            ->setCellValue('D'.$row, $value->mail)
+            ->setCellValue('E'.$row, $value->mission_id)
+            ->setCellValue('F'.$row, $value->state)
+            ->setCellValue('G'.$row, $value->manager_id);
+        $row++;     
+}
+$sheet->setCellValue('A1', 'Liste des Consultants')
+        ->setCellValue('B1', 'Nom')
+        ->setCellValue('C1', 'Prénom')
+        ->setCellValue('D1', 'Mail')
+        ->setCellValue('E1', 'Mission')
+        ->setCellValue('F1', 'Statut')
+        ->setCellValue('G1', 'Manager');
+$sheet->mergeCells('A1:D1');
 $sheet->setTitle('Consultant');
 
-$worksheet2 = $spreadsheet->createSheet();
-$worksheet2->setTitle('Mission');
+$sql = 'SELECT * FROM mission';
+$statement = $connection->prepare($sql);
+$statement->execute();
+$missions = $statement->fetchAll(PDO::FETCH_OBJ);
+$spreadsheet->createSheet();
 
-$worksheet3 = $spreadsheet->createSheet();
-$worksheet3->setTitle('Client');
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment; filename="Akkappiness.xlsx"');
+header('Cache-Control: max-age=0');
+header('Cache-Control: max-age=1');
+header('Cache-Control: cache, must-revalidate');
+header('Pragma: public');
 
-$worksheet4 = $spreadsheet->createSheet();
-$worksheet4->setTitle('Enquête');
+$writer = new Xlsx($spreadsheet);
+$writer->save('php://output');
+?>
+
 /* /**  Loop through all the remaining files in the list  **/
 //foreach($inputFileNames as $sheet => $inputFileName) {
     /**  Increment the worksheet index pointer for the Reader  **/
@@ -40,11 +59,3 @@ $worksheet4->setTitle('Enquête');
     /**  Set the worksheet title (to the filename that we've loaded)  **/
     //$spreadsheet->getActiveSheet()
         //->setTitle(pathinfo($inputFileName,PATHINFO_BASENAME));*/
-
-// Redirect output to a client
-header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment; filename="Akkappiness.xlsx"');
-
-$writer = new Xlsx($spreadsheet);
-$writer->save('php://output');
-?>
