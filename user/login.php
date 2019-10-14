9c4e7f7e4817586d39be3874ea4341159b8e8892
VASSEUR Maxime <Maxime.VASSEUR@akka.eu>
2019-10-14 09:49:33 +0200

    login

--- user/login.php
+++ user/login.php
@@ -2,27 +2,8 @@
 <html lang="fr">
 <?php require '../layout/header.php'; ?>
 
-<body>
-  <div action="login.php" class="container login col-lg-4 col-sm-6 text-center">
-    <form method="post">
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email</label>
-        <input type="email" name="login" class="form-control" value="<?php if (isset($_POST['login'])) echo htmlentities(trim($_POST['login'])); ?>" placeholder="Enter email">
-      </div>
-      <div class="form-group mt-4">
-        <label for="exampleInputPassword1">Mot de passe</label>
-        <input type="password" name="pass" class="form-control" value="<?php if (isset($_POST['pass'])) echo htmlentities(trim($_POST['pass'])); ?>" placeholder="Password">
-      </div>
-      <div class="form-group">
-        <button type="submit" class="btn btn-info connexion" name="connexion" value="Connexion">Connexion</button>
-      </div>
-    </form>
-  </div>
-
-  <?php require '../layout/footer.php'; ?>
-  <?php
+<?php
   session_start();
-  require '../db/db.php';
 
   if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['pass']) && !empty($_POST['pass']))) {
 
@@ -41,6 +22,25 @@
     exit();
   }
   ?>
+<body>
+  <div action="login.php" class="container login col-lg-4 col-sm-6 text-center">
+    <form method="post">
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email</label>
+        <input type="email" name="login" class="form-control" value="<?php if (isset($_POST['login'])) echo htmlentities(trim($_POST['login'])); ?>" placeholder="Enter email">
+      </div>
+      <div class="form-group mt-4">
+        <label for="exampleInputPassword1">Mot de passe</label>
+        <input type="password" name="pass" class="form-control" value="<?php if (isset($_POST['pass'])) echo htmlentities(trim($_POST['pass'])); ?>" placeholder="Password">
+      </div>
+      <div class="form-group">
+        <button type="submit" class="btn btn-info connexion" name="connexion" value="Connexion">Connexion</button>
+      </div>
+    </form>
+  </div>
+
+  <?php require '../layout/footer.php'; ?>
+  
 </body>
 
 </html>
