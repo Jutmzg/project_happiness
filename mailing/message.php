d0828ebb0fe34ad2fa9b19e9295ebb417caf5d5e
BOUSSEAU Juliette <Juliette.BOUSSEAU@akka.eu>
2019-10-14 11:54:41 +0200

    mail

--- mailing/message.php
+++ mailing/message.php
@@ -1,19 +1,19 @@
 <?php
 
-$to = 'jul.bousseau@gmail.com';
+/*$to = 'jul.bousseau@gmail.com';
 $subject = 'test';
 $from = 'jul.bousseau@gmail.com';
-/*$headers = "";
+$headers = "";
 $headers = "From: " . strip_tags($_POST['req-email']) . "\r\n";
 $headers .= "Reply-To: ". strip_tags($_POST['req-email']) . "\r\n";
 $headers .= "From: jul.bousseau@gmail.com\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";*/
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-$message = 'testez';
+$message = 'testez';*/
 
 // Sending mail
-if(mail($to, $subject, $message)){
+if(mail('jul.bousseau@gmail.com', 'test mail', 'test')){
     echo 'Le mail a bien été envoyé !';
 } else{
     echo 'Impossible d\'envoyer le mail. Veuillez réessayer.';
