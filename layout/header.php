281580f80e641f34e8a794ee5b470e878dcab561
VASSEUR Maxime <Maxime.VASSEUR@akka.eu>
2019-10-16 11:43:18 +0200

    ajout blocage dans header

--- layout/header.php
+++ layout/header.php
@@ -1,6 +1,14 @@
 
 <?php require_once('../connection.php'); ?>
 
+<?php
+  if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+    if (!isset($_SESSION['login'])) {
+      header('Location: /Akkappiness/user/login.php');
+    }
+  }; ?>
+
 <head>
   <title>AKKAPPINESS</title>
 
