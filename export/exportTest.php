c467b002e66c8644c58f6bad3a7fed5e381fa78a
BOUSSEAU Juliette <Juliette.BOUSSEAU@akka.eu>
2019-10-22 10:00:47 +0200

    export variable condition

--- export/exportTest.php
+++ export/exportTest.php
@@ -4,34 +4,25 @@ require '../connection.php';
 $sql = 'SELECT m.name as mission, resultat, created_at, e.state AS state FROM enquete AS e LEFT JOIN mission AS m ON m.id=e.mission_id';
 $statement = $connection->prepare($sql);
 $statement->execute();
-$enquetes = $statement->fetchAll(PDO::FETCH_ASSOC);
+$enquetes = $statement->fetchAll(PDO::FETCH_OBJ);
 
-/* $enquete[0]['resultat'] = 'Pas de retour';
-$enquete[1]['resultat'] = 'Bon';
-$enquete[2]['resultat'] = 'Moyen';
-$enquete[3]['resultat'] = 'Mauvais'; */
 
 
 $excel = "";
 $excel .=  "MISSION\tRESULTAT\tCREE LE\tENVOYE\n";
 
 foreach($enquetes as $enquete) {
-    switch ($enquete) {
-        case 0:
-            $answer = 'Pas de réponse';
-            break;
-        case 1:
-            $answer = 'Bien';
-            break;
-        case 2:
-            $answer = 'Moyen';
-            break;
-        case 3:
-            $answer = 'Mauvais';
-            break;
-    }    
-        $excel .= "$enquete[mission]\t$enquete[resultat]\t$enquete[created_at]\t$enquete[state]\n";
-        $answer = ''; 
+    
+    $excel .= "$enquete->mission\t$enquete->resultat\t$enquete->created_at\t$enquete->state\n";
+    if($enquete->resultat === '0'){
+            echo "Pas de retour";
+    } elseif ($enquete->resultat === '1'){
+            echo "Bon";
+    } elseif ($enquete->resultat === '2'){
+            echo "Moyen";
+    } elseif ($enquete->resultat === '3'){
+            echo "Mauvais";
+    } 
 }   
 
 header("Content-type: application/vnd.msexcel; charset=utf-16");
