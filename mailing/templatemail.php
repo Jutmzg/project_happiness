894a6879e3e8fc79bae7ad21b0c9a6e6073cf541
VASSEUR Maxime <Maxime.VASSEUR@akka.eu>
2019-10-23 10:13:07 +0200

    template mail

---
+++ mailing/templatemail.php
@@ -0,0 +1,95 @@
+<table align="center" bgcolor="#EAECED" border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tbody>
+        <tr>
+            <td align="center" valign="bottom">
+                <table width="600">
+                    <tbody>
+                        <tr>
+                            <td align="center" valign="bottom">
+                                <table border="0" cellpadding="0" cellspacing="0" width="580">
+                                    <tbody>
+                                        <tr>
+                                            <td align="right" valign="bottom">
+                                                <a href="http://3littlemudpies.com" style="font-family:'Open Sans',arial,sans-serif!important;font-weight:200!important;font-size:12px;color:#989ca5!important;padding:1px 0;border-bottom:1px solid #bfc4c8;text-decoration:none!important" target="_blank">View in
+                                                    browser</a>
+                                            </td>
+                                        </tr>
+
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td align="center" valign="top">
+                <table bgcolor="#FFFFFF" border="0" cellpadding="0" cellspacing="0" style="overflow:hidden!important;border-radius:3px" width="580">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <a href="http://get.invisionapp.com/e1t/c/*W8q197m87bTZDW3GQmLK13hZky0/*W78sCb-6TQXb4W7MqF0m7ChH5m0/5/f18dQhb0SbTJ8XJ9l8W88T8pQ2qwv1yN32hfBSK3Lt3Vf5c9R57mvC2W1BQYf65LMjn_W7tZ_DP8hvPPsW79LbD78mQCyLW5rGt248XWFN4W5ZBHl08l0qQlW7b9Gbn7N_LRjVJd2Tj9crycyW7JtTqn8mnw90W2wpQW71h4tZyW85D82T1Z69g3W8hTJV48lwVXYW8hS2967bj1-tW2zfyqd7NrMZsW3ndfYD4W9-mlVHMtf-2Bzx8zMMNBCZMMQxXW80gSRf97rB1bW8xqQbR4VK71KN8n7G9QSRRb9VXN3cB60MWLwW3brxmC3GlyL6W5G76cr8lQrRVN8p_3wpPT53lW5xVjR93d9wLwW5vf-d41q8CM9N4psflPZCyWnVPCvdr5tPyC4W6RW_7d5w8mWcN96tbdkgRk0KW7wTZy29jW8Z3N8csGDVD2JF5W7jWX0j34Yf6YW5sGrVc5DVmZwW2sG3VL1b--xsW2GD9PD47Tc7tW9hxYGc95RlbHVbSwxV4sGfFwN1WBdMHH3BXYW7Zzsnm4L1MlSW4HzlXQ65LYMR111" target="_blank"><img src="https://ci3.googleusercontent.com/proxy/CZ0wbcOk5RizUmmwBTrOqXyAZj8RukfNwrVL2si5Iqry5G8UVcg4cOOgJK7mMe3tHXX2SUt2XaSOLFX11ckb_1MU5jboC34jtt9CjKrp6TIgScoSL98XIpckIWMImecpqIVuljl3qyjrl4YkWwWJTCrRbrlYPIbeYL-e5Uws=s0-d-e1-ft#http://get.invisionapp.com/hs-fs/hubfs/2015_Year_In_Review/workflow.gif?width=&amp;t=1452605472977&amp;width=580" style="border:0;max-width:100%!important" width="580"></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td align="center">
+                                <table width="85%">
+                                    <tbody>
+                                        <tr>
+                                            <td align="center">
+                                                <h2 style="margin:0!important;font-family:'Open Sans',arial,sans-serif!important;font-size:28px!important;line-height:38px!important;font-weight:200!important;color:#252b33!important">
+                                                    Workflow</h2>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td align="center" style="font-family:'Open Sans',arial,sans-serif!important;font-size:28px!important;line-height:38px!important;font-weight:400!important;color:#252b33!important;font-style:italic!important">
+                                                Design-driven
+                                                project
+                                                management</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td align="center">
+                                <table border="0" cellpadding="0" cellspacing="0" width="78%">
+                                    <tbody>
+                                        <tr>
+                                            <td align="center" style="font-family:'Open Sans',arial,sans-serif!important;font-size:16px!important;line-height:30px!important;font-weight:400!important;color:#7e8890!important">
+                                                Text</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td align="center" valign="top">
+                                <table border="0" cellpadding="0" cellspacing="0">
+                                    <tbody>
+                                        <tr>
+                                            <td align="center" valign="top">
+                                                <a href="http://get.invisionapp.com/e1t/c/*W8q197m87bTZDW3GQmLK13hZky0/*W4mFbXx3sXRhYVnPSMq4gP1sj0/5/f18dQhb0SbTJ8XJ9l8W88T8pQ2qwv1yN32hfBSK3Lt3Vf5c9R57mvC2W1BQYf65LMjn_W7tZ_DP8hvPPsW79LbD78mQCyLW5rGt248XWFN4W5ZBHl08l0qQlW7b9Gbn7N_LRjVJd2Tj9crycyW7JtTqn8mnw90W2wpQW71h4tZyW85D82T1Z69g3W8hTJV48lwVXYW8hS2967bj1-tW2zfyqd7NrMZsW3ndfYD4W9-mlVHMtf-2Bzx8zMMNBCZMMQxXW80gSRf97rB1bW8yy3yZ35WBcKW4pXj982CM6L_W1mHwXn2CmXwMW7gvdtn5yYfYnW3388Cj3DLHwqW4MC7M79m2DlBW2ZF0NN3wPdc6W31wVY8953GpyW6J51dw7LDsLPN52yyVpyV79bW8XzVB56cMFYTW828Ncb5gSHY5W6H5W1X4y_n9zVVR6H24q1sj1W1l8Gzb5nJ89-W52Hlx34J1Y40VhdTwP4zFKY_W4r5l5j5d0ZB9W1qkh1p3Qr_s4W8Mw0ZJ3sqktdW6_VStj8vTk_tVRdDtK3QzS4HSbts9lk_0_102" style="background-color:#ff2b63;padding:14px 28px 14px 28px;border-radius:3px;line-height:18px!important;letter-spacing:0.125em;text-transform:uppercase;font-size:13px;font-family:'Open Sans',Arial,sans-serif;font-weight:400;color:#ffffff;text-decoration:none;display:inline-block;line-height:18px!important" target="_blank">MAKE
+                                                    IT WORK</a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+
+
+                </table>
